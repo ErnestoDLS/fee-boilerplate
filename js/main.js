92a24ca41b7bc5e2305d24e93a662cacc46ab7f5
Ernesto De Los Santos <netos_id@yahoo.com>
2016-04-26 16:40:30 -0500

    Have all repos pulled.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,38 @@
 (function() {
-  // All code NOT referencing DOM elements can go here
+  var reposXHR = new XMLHttpRequest();
+  var githubProfileXHR = new XMLHttpRequest();
+
+  var ownerName = document.querySelector("[data-js='my__name']");
+  var ownerBlog = document.querySelector("[data-js='my__blog']");
+  var ownerLocation = document.querySelector("[data-js='my__location']");
+  var ownerEmail = document.querySelector("[data-js='my__email']");
+  var ownerAvatar = document.querySelector("[data-js='my__picture']");
+  
+
   document.addEventListener("DOMContentLoaded", function(e){
-    // ALL DOM RELATED QUERYING GOES HERE
+    var repoOwnerElement = document.querySelector("[data-js='repo__owner']");
+    var repoNavElement = document.querySelector("[data-js='repo__navigation']");
+
+    reposXHR.addEventListener("load", function(e){
+      var data = JSON.parse(e.target.responseText);
+      data.forEach(function(repo){
+        var html = "";
+        html += "<a class='repo__navItem' href='" + repo.http_url + "'>" + repo.name + "</a>";
+        repoNavElement.innerHTML += html;
+      });
+    });
+
+    githubProfileXHR.addEventListener("load", function(e){
+      var profileData = JSON.parse(e.target.responseText);
+      repoOwnerElement.textContent = profileData.name;
+    });
   });
 
-}());
+
+  reposXHR.open('GET',  "https://api.github.com/users/ErnestoDLS/repos");
+  reposXHR.send();
+
+  githubProfileXHR.open('GET',  "https://api.github.com/users/ErnestoDLS/repos");
+  githubProfileXHR.send();
+
+})();
