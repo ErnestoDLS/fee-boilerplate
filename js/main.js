e166a4a40ebd88afb2a55683bcb9122768162e20
Ernesto De Los Santos <netos_id@yahoo.com>
2016-04-26 22:03:54 -0500

    finished adding vars.

--- js/main.js
+++ js/main.js
@@ -1,17 +1,25 @@
 (function() {
   var reposXHR = new XMLHttpRequest();
   var githubProfileXHR = new XMLHttpRequest();
+  var nameXHR = new XMLHttpRequest();
 
-  var ownerName = document.querySelector("[data-js='my__name']");
-  var ownerBlog = document.querySelector("[data-js='my__blog']");
-  var ownerLocation = document.querySelector("[data-js='my__location']");
-  var ownerEmail = document.querySelector("[data-js='my__email']");
-  var ownerAvatar = document.querySelector("[data-js='my__picture']");
-  
 
   document.addEventListener("DOMContentLoaded", function(e){
     var repoOwnerElement = document.querySelector("[data-js='repo__owner']");
     var repoNavElement = document.querySelector("[data-js='repo__navigation']");
+    var ownerName = document.querySelector("[data-js='my__name']");
+    var ownerBlog = document.querySelector("[data-js='my__blog']");
+    var ownerLocation = document.querySelector("[data-js='my__location']");
+    var ownerEmail = document.querySelector("[data-js='my__email']");
+    var ownerAvatar = document.querySelector("[data-js='my__picture']");
+
+    nameXHR.addEventListener("load", function(e){
+      var data = JSON.parse(e.target.responseText);
+      data.forEach(function(name){
+        var ownerName = "";
+        ownerName += "data-js='my__name'" + name.full_name;
+        ownerName.textContent += text;
+
 
     reposXHR.addEventListener("load", function(e){
       var data = JSON.parse(e.target.responseText);
